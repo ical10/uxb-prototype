c83d0e0e77d8f6c87f8ee4f7a4552ddba4ad2ba7
ical10 <mochhusnir@gmail.com>
2025-07-04 23:13:25 +0800

    fix: move top level await fn

--- registry/new-york/blocks/complex-component/lib/block-number.ts
+++ registry/new-york/blocks/complex-component/lib/block-number.ts
@@ -1,17 +1,17 @@
 import { DedotClient, WsProvider } from 'dedot';
 import type { PolkadotApi } from '@dedot/chaintypes';
 
-// Initialize providers & clients
-const provider = new WsProvider('wss://rpc.polkadot.io');
-const client = await DedotClient.new<PolkadotApi>(provider);
-
 type BlockNumberProps = {
     unsub: () => void;
     blockNumber: number;
 }
 
 export async function getBlockNumber(onBlockNumberUpdate?: (blockNumber: number) => void): Promise<BlockNumberProps | null> {
+    const provider = new WsProvider('wss://rpc.polkadot.io');
+
     try {
+        const client = await DedotClient.new<PolkadotApi>(provider);
+        
         const currentBlockNumber = await client.query.system.number();
         
         const unsub = await client.query.system.number((blockNumber) => {
